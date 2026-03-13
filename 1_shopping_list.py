b2202ad3e5871d8ddeaa774e20bbf6df908108bc
acastinaar2002-sys <a.castinaar@gmail.com>
2026-03-12 19:26:13 -0500

    Realice un input para solicitar al usuario que agregue un producto nuevo a la lista

--- 1_shopping_list.py
+++ 1_shopping_list.py
@@ -1,9 +1,13 @@
 def shopping_list() -> None:
 
-    shopping_list = ["pan", "leche", "huevos", "jugo de naranja", "carne guisada"]
+    shopping_list = ["Botella de Ron", "Botella de vodka", "Huevos", "Jugo de naranja", "Jugo de Cranberry"]
     print("Lista de compras: ") 
     for product in shopping_list:
         print(product) 
-
+    add_product = input("Agrega un producto a la lista de compras: ")
+    shopping_list.append(add_product) 
+    print("\nLista actualizada:")
+    for product in shopping_list:
+        print(product)
 
 shopping_list()
